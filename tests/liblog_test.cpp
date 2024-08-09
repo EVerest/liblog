8365d5b23e2d854207370127bb4105944198da64
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2024-08-09 11:17:42 +0200

    Fix gtest integration, add some first unit tests, fix coverage generation
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- tests/liblog_test.cpp
+++ tests/liblog_test.cpp
@@ -1,9 +1,31 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright 2020 - 2023 Pionix GmbH and Contributors to EVerest
+// Copyright Pionix GmbH and Contributors to EVerest
+#include <exception>
+#include <mutex>
+
 #include <gtest/gtest.h>
+#include <spdlog/sinks/base_sink.h>
+
+#include <everest/logging.hpp>
 
 namespace Everest {
 namespace Logging {
+
+class TestSink : public spdlog::sinks::base_sink<std::mutex> {
+public:
+    // return number of log messages logged
+    size_t count() {
+        return this->counter;
+    }
+
+protected:
+    void sink_it_(const spdlog::details::log_msg& message) override {
+        this->counter += 1;
+    }
+
+    void flush_() override {}
+    size_t counter = 0;
+};
 
 class LibLogUnitTest : public ::testing::Test {
 protected:
@@ -14,8 +36,37 @@ protected:
     }
 };
 
-TEST(LibLogUnitTest, test_truth) {
-    ASSERT_TRUE(1 == 1);
+TEST_F(LibLogUnitTest, test_log_source_file_linenr_function) {
+    auto log_source = LogSource("file", 42, "function");
+    EXPECT_TRUE(log_source.log_file);
+    EXPECT_TRUE(log_source.log_line);
+    EXPECT_TRUE(log_source.log_function);
+}
+
+TEST_F(LibLogUnitTest, test_log_source_function) {
+    auto log_source = LogSource("function");
+    EXPECT_FALSE(log_source.log_file);
+    EXPECT_FALSE(log_source.log_line);
+    EXPECT_TRUE(log_source.log_function);
+}
+
+TEST_F(LibLogUnitTest, test_evthrow) {
+    ASSERT_THROW(EVTHROW(std::runtime_error("hello there")), std::runtime_error);
+}
+
+TEST_F(LibLogUnitTest, test_evlog_and_throw) {
+    ASSERT_THROW(EVLOG_AND_THROW(std::runtime_error("hello there")), std::runtime_error);
+}
+
+TEST_F(LibLogUnitTest, test_everest_logger) {
+    auto test_sink = std::make_shared<TestSink>();
+    auto test_logger = std::make_shared<spdlog::logger>("", test_sink);
+    spdlog::set_default_logger(test_logger);
+    spdlog::set_level(spdlog::level::level_enum::trace);
+    EVLOG_verbose << "hello there";
+    EVLOG_info << "hello there";
+    EVLOG_warning << "hello there";
+    ASSERT_EQ(test_sink->count(), 3);
 }
 
 } // namespace Logging
