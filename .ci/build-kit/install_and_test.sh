0abde84e383f9e991e67cb9b07d34ffdd9778211
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2024-08-15 17:04:35 +0200

    Rewrite trap
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- .ci/build-kit/install_and_test.sh
+++ .ci/build-kit/install_and_test.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+copy_logs() {
+    cp build/Testing/Temporary/LastTest.log /ext/ctest-report
+    cp -R build/everest-log_gcovr_coverage /ext/gcovr_coverage
+}
+
 cmake \
     -B build \
     -S "$EXT_MOUNT/source" \
@@ -12,10 +17,12 @@ cmake \
 
 ninja -j$(nproc) -C build install
 
-trap "cp build/Testing/Temporary/LastTest.log /ext/ctest-report && cp -R build/everest-log_gcovr_coverage /ext/gcovr_coverage" EXIT
+trap "copy_logs" ERR
 
 ninja -j$(nproc) -C build test
 
 pip install gcovr==6
 
 ninja -j$(nproc) -C build everest-log_gcovr_coverage
+
+copy_logs
