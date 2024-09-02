501a003a78e1b7772ba23f1d8bc3b98fdea81e7a
Andreas Heinrich <andreas.heinrich@rwth-aachen.de>
2024-09-02 15:43:02 +0200

    Update CI
    * Add `build-kit-liblog`
    * Use newer `build-kit-base`
    * Split up scripts
    * Refactor `build_and_test` workflow file
    
    Signed-off-by: Andreas Heinrich <andreas.heinrich@rwth-aachen.de>

---
+++ .ci/build-kit/scripts/run_coverage.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+ninja -C build everest-log_gcovr_coverage
+retVal=$?
+
+# Copy the generated coverage report to the mounted directory in any case
+cp -R "$EXT_MOUNT/build/everest-log_gcovr_coverage" "$EXT_MOUNT/gcovr-coverage"
+
+if [ $retVal -ne 0 ]; then
+    echo "Coverage failed with return code $retVal"
+    exit $retVal
+fi
