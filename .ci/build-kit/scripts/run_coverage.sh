251ecf6a048a473866fe3bc5524ed0ce531710c7
Andreas Heinrich <andreas.heinrich@rwth-aachen.de>
2024-09-10 17:23:15 +0200

    tmp print lasttestlog
    Signed-off-by: Andreas Heinrich <andreas.heinrich@rwth-aachen.de>

--- .ci/build-kit/scripts/run_coverage.sh
+++ .ci/build-kit/scripts/run_coverage.sh
@@ -5,10 +5,14 @@ ninja \
     everest-log_gcovr_coverage
 retValHTML=$?
 
+cat "$EXT_MOUNT/build/Testing/Temporary/LastTest.log"
+
 ninja \
     -C "$EXT_MOUNT/build" \
     everest-log_gcovr_coverage_xml
 retValXML=$?
+
+cat "$EXT_MOUNT/build/Testing/Temporary/LastTest.log" 
 
 # Copy the generated coverage report and xml to the mounted directory in any case
 cp -R "$EXT_MOUNT/build/everest-log_gcovr_coverage" "$EXT_MOUNT/gcovr-coverage"
