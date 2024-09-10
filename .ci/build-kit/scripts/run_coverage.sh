40b13c9728a712e20815628c00637a2fce1aa9ec
Andreas Heinrich <andreas.heinrich@rwth-aachen.de>
2024-09-10 17:06:42 +0200

    fix run_coverage script
    Signed-off-by: Andreas Heinrich <andreas.heinrich@rwth-aachen.de>

--- .ci/build-kit/scripts/run_coverage.sh
+++ .ci/build-kit/scripts/run_coverage.sh
@@ -2,15 +2,24 @@
 
 ninja \
     -C "$EXT_MOUNT/build" \
-    everest-log_gcovr_coverage \
+    everest-log_gcovr_coverage
+retValHTML=$?
+
+ninja \
+    -C "$EXT_MOUNT/build" \
     everest-log_gcovr_coverage_xml
-retVal=$?
+retValXML=$?
 
 # Copy the generated coverage report and xml to the mounted directory in any case
 cp -R "$EXT_MOUNT/build/everest-log_gcovr_coverage" "$EXT_MOUNT/gcovr-coverage"
 cp "$EXT_MOUNT/build/everest-log_gcovr_coverage_xml.xml" "$EXT_MOUNT/gcovr-coverage-xml.xml"
 
-if [ $retVal -ne 0 ]; then
-    echo "Coverage failed with return code $retVal"
-    exit $retVal
+if [ $retValHTML -ne 0 ]; then
+    echo "Coverage HTML report failed with return code $retValHTML"
+    exit $retValHTML
+fi
+
+if [ $retValXML -ne 0 ]; then
+    echo "Coverage XML report failed with return code $retValXML"
+    exit $retValXML
 fi
