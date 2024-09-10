c64e9f87d88bb9f012fec41fd075b7b5ebb7ea18
Andreas Heinrich <andreas.heinrich@rwth-aachen.de>
2024-09-10 14:49:16 +0200

    fix cp command
    Signed-off-by: Andreas Heinrich <andreas.heinrich@rwth-aachen.de>

--- .ci/build-kit/scripts/run_coverage.sh
+++ .ci/build-kit/scripts/run_coverage.sh
@@ -8,7 +8,7 @@ retVal=$?
 
 # Copy the generated coverage report and xml to the mounted directory in any case
 cp -R "$EXT_MOUNT/build/everest-log_gcovr_coverage" "$EXT_MOUNT/gcovr-coverage"
-cp -R "$EXT_MOUNT/build/everest-log_gcovr_coverage_xml.xml" "$EXT_MOUNT/gcovr-coverage-xml.xml"
+cp "$EXT_MOUNT/build/everest-log_gcovr_coverage_xml.xml" "$EXT_MOUNT/gcovr-coverage-xml.xml"
 
 if [ $retVal -ne 0 ]; then
     echo "Coverage failed with return code $retVal"
