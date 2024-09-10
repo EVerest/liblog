e9557582cf0e7d910f68a29e48c2d5bca0823901
Andreas Heinrich <andreas.heinrich@rwth-aachen.de>
2024-09-10 14:26:59 +0200

    add xml coverage file
    Signed-off-by: Andreas Heinrich <andreas.heinrich@rwth-aachen.de>

--- .ci/build-kit/scripts/run_coverage.sh
+++ .ci/build-kit/scripts/run_coverage.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-ninja -C "$EXT_MOUNT/build" everest-log_gcovr_coverage
+ninja \
+    -C "$EXT_MOUNT/build" \
+    everest-log_gcovr_coverage \
+    everest-log_gcovr_coverage_xml
 retVal=$?
 
-# Copy the generated coverage report to the mounted directory in any case
+# Copy the generated coverage report and xml to the mounted directory in any case
 cp -R "$EXT_MOUNT/build/everest-log_gcovr_coverage" "$EXT_MOUNT/gcovr-coverage"
+cp -R "$EXT_MOUNT/build/everest-log_gcovr_coverage_xml.xml" "$EXT_MOUNT/gcovr-coverage-xml.xml"
 
 if [ $retVal -ne 0 ]; then
     echo "Coverage failed with return code $retVal"
